0247e0a2ba9720d318db7faf8cff4ec14def3c5e
Allanoud <alrifaialanoud@gmail.com>
2024-03-04 12:02:34 +0300

    Update gwz_sales.sql

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,3 +1,5 @@
 SELECT date_date,
-ROUND (SUM(turnover),1) AS turnover
- FROM `data-analytics-bootcamp-363212.course14.gwz_sales`
+ROUND (SUM(turnover),1) AS turnover,
+ROUND (SUM(purchase_cost),1) AS purchase_cost
+FROM `data-analytics-bootcamp-363212.course14.gwz_sales`
+GROUP BY date_date
