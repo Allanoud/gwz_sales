b42ec694232234c66b249e60f77ed9668014cf0f
Allanoud <alrifaialanoud@gmail.com>
2024-03-04 12:48:45 +0300

    add_purchase_cost

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,5 +1,6 @@
-SELECT date_date,
+SELECT date_date, purchase_cost
 ROUND (SUM(turnover),1) AS turnover,
 ROUND (SUM(purchase_cost),1) AS purchase_cost
+
 FROM `data-analytics-bootcamp-363212.course14.gwz_sales`
 GROUP BY date_date
